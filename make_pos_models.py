60a8158d4844dae8b4b9e2f3a21f6e8e3bd629bb
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-07-08 23:47:13 -0700

    add new model making script

--- make_pos_models.py
+++ make_pos_models.py
@@ -1,3 +1,5 @@
+"""Train cltk POS models from a training set and save them in the model repository."""
+
 from nltk.corpus.reader import TaggedCorpusReader
 from nltk.tag import UnigramTagger
 from nltk.tag import BigramTagger
@@ -5,9 +7,12 @@ from nltk.tag import TrigramTagger
 from nltk.tag import tnt
 import os
 import pickle
+import time
 
 
 def make_pos_model(model_type):
+    now = time.time()
+
     reader = TaggedCorpusReader('.', 'latin_training_set.pos')
     train_sents = reader.tagged_sents()
     if model_type == 'unigram':
@@ -16,18 +21,17 @@ def make_pos_model(model_type):
     elif model_type == 'bigram':
         tagger = BigramTagger(train_sents)
         file = 'bigram.pickle'
-    elif model_type == 'trigam':
+    elif model_type == 'trigram':
         tagger = TrigramTagger(train_sents)
         file = 'trigram.pickle'
     elif model_type == 'backoff':
-        with open('ungigram.pickle', 'rb') as opened_pickle:
-            tagger1 = pickle.load(opened_pickle)
+        tagger1 = BigramTagger(train_sents)
         tagger2 = BigramTagger(train_sents, backoff=tagger1)
         tagger = TrigramTagger(train_sents, backoff=tagger2)
         file = '123grambackoff.pickle'
     elif model_type == 'tnt':
         tagger = tnt.TnT()
-        tagger = tagger.train(train_sents)
+        tagger.train(train_sents)
         file = 'tnt.pickle'
     else:
         print('Invalid model_type.')
@@ -36,6 +40,8 @@ def make_pos_model(model_type):
     path = os.path.join(_dir, file)
     with open(path, 'wb') as f:
         pickle.dump(tagger, f)
+
+    print('Completed training {0} model in {1} seconds to {2}.'.format(model_type, time.time() - now, path))
 
 if __name__ == "__main__":
     make_pos_model('unigram')
